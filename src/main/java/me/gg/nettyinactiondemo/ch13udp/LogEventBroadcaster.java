bac4d0970d29e27f268079c0944849a51b6df494
cg <superwcg@gmail.com>
2019-01-30 00:11:53 +0800

    upd test ok.

---
+++ src/main/java/me/gg/nettyinactiondemo/ch13udp/LogEventBroadcaster.java
@@ -0,0 +1,89 @@
+package me.gg.nettyinactiondemo.ch13udp;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+
+/**
+ * Listing 13.3 LogEventBroadcaster
+ *
+ * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
+ */
+public class LogEventBroadcaster {
+    private final EventLoopGroup group;
+    private final Bootstrap bootstrap;
+    private final File file;
+
+    public LogEventBroadcaster(InetSocketAddress address, File file) {
+        group = new NioEventLoopGroup();
+        bootstrap = new Bootstrap();
+        bootstrap.group(group).channel(NioDatagramChannel.class)
+             .option(ChannelOption.SO_BROADCAST, true)
+             .handler(new LogEventEncoder(address));
+        this.file = file;
+    }
+
+    public void run() throws Exception {
+        Channel ch = bootstrap.bind(0).sync().channel();
+        long pointer = 0;
+        for (;;) {
+            long len = file.length();
+            if (len < pointer) {
+                // file was reset
+                pointer = len;
+            } else if (len > pointer) {
+                // Content was added
+                RandomAccessFile raf = new RandomAccessFile(file, "r");
+                raf.seek(pointer);
+                String line;
+                while ((line = raf.readLine()) != null) {
+                    line = new String(line.getBytes("ISO-8859-1"),"UTF-8");
+                    System.out.println(line);
+                    ch.writeAndFlush(new LogEvent(null, -1, file.getAbsolutePath(), line));
+
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        Thread.interrupted();
+                        break;
+                    }
+                }
+//                pointer = raf.getFilePointer();
+                raf.close();
+            }
+//            try {
+//                Thread.sleep(3000);
+//            } catch (InterruptedException e) {
+//                Thread.interrupted();
+//                break;
+//            }
+        }
+    }
+
+    public void stop() {
+        group.shutdownGracefully();
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            throw new IllegalArgumentException();
+        }
+        LogEventBroadcaster broadcaster = new LogEventBroadcaster(
+//                new InetSocketAddress("255.255.255.255", Integer.parseInt(args[0])), new File(args[1]));
+                new InetSocketAddress("127.0.0.1", Integer.parseInt(args[0])), new File(args[1]));
+        try {
+            broadcaster.run();
+        }
+        finally {
+            broadcaster.stop();
+        }
+    }
+
+}
